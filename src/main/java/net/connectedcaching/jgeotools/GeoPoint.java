35857e1c2b27c04e1b35bbddd2beca69d2fdaf2e
Leo Schweizer <leonhard.schweizer@googlemail.com>
2012-09-29 14:12:33 +0200

    added conversion to DMS

--- src/main/java/net/connectedcaching/jgeotools/GeoPoint.java
+++ src/main/java/net/connectedcaching/jgeotools/GeoPoint.java
@@ -1,5 +1,46 @@
 package net.connectedcaching.jgeotools;
 
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+import java.util.Locale;
+
 public class GeoPoint {
+
+	protected Double latitude;
+	protected Double longitude;
+
+	private GeoPoint() {}
+
+	public static GeoPoint parse(Double latitude, Double longitude) {
+		GeoPoint point = new GeoPoint();
+		point.latitude = latitude;
+		point.longitude = longitude;
+		return point;
+	}
+
+	private String decimalToDms(Double coordinate, Locale locale) {
+		DecimalFormat df = (DecimalFormat) NumberFormat.getInstance(locale);
+		df.applyPattern("#.###");
+		coordinate = Math.abs(coordinate);
+		Double fractionalPart = coordinate % 1;
+		String degrees = String.valueOf(coordinate.intValue());
+		coordinate = fractionalPart * 60;
+		fractionalPart = coordinate % 1;
+		String minutes = String.valueOf(coordinate.intValue());
+		coordinate = fractionalPart * 60;
+		return String.format("%s° %s' %s\"", degrees, minutes, df.format(coordinate));
+	}
+
+	public String asDms() {
+		return asDms(Locale.getDefault());
+	}
+
+	public String asDms(Locale locale) {
+		String latitudeDirection = latitude > 0 ? "N" : "S";
+		String longitudeDirection = longitude > 0 ? "E" : "W";
+		return String.format("%s%s %s%s",
+			latitude != 0 ? latitudeDirection : "", decimalToDms(latitude, locale),
+			longitude != 0 ? longitudeDirection : "", decimalToDms(longitude, locale));
+	}
 
 }
--- src/test/scala/net/connectedcaching/jgeotools/tests/GeoPointSpec.scala
+++ src/test/scala/net/connectedcaching/jgeotools/tests/GeoPointSpec.scala
@@ -1,23 +1,53 @@
 package net.connectedcaching.jgeotools.tests
 
 import org.specs2.mutable._
+import net.connectedcaching.jgeotools.GeoPoint
+import java.util.Locale
 
-class GeoPointSpec extends Specification {
+class GeoPointSpec extends Specification with BeforeAfter {
 
-	"The 'Hello world' string" should {
-	
-		"contain 11 characters" in {
-			"Hello world" must have size(11)
+	val defaultLocale = Locale.getDefault
+
+	def before {
+		Locale.setDefault(Locale.ENGLISH)
+	}
+
+	"GeoPoint to DMS (degrees, minutes, seconds) conversion" should {
+
+		"work correctly for NE coordinates" in {
+			val geoPoint = GeoPoint.parse(46.414167, 6.927500)
+			geoPoint.asDms must be equalTo("N46° 24' 51.001\" E6° 55' 39\"")
 		}
-		
-		"start with 'Hello'" in {
-			"Hello world" must startWith("Hello")
+
+		"work correctly for SE coordinates" in {
+			val geoPoint = GeoPoint.parse(-33.855270, 151.209725)
+			geoPoint.asDms must be equalTo("S33° 51' 18.972\" E151° 12' 35.01\"")
 		}
-		
-		"end with 'world'" in {
-			"Hello world" must endWith("world")
+
+		"work correctly for NW coordinates" in {
+			val geoPoint = GeoPoint.parse(50.116973, -122.945424)
+			geoPoint.asDms must be equalTo("N50° 7' 1.103\" W122° 56' 43.526\"")
 		}
-		
+
+		"work correctly for SW coordinates" in {
+			val geoPoint = GeoPoint.parse(-14.170074, -141.236336)
+			geoPoint.asDms must be equalTo("S14° 10' 12.266\" W141° 14' 10.81\"")
+		}
+
+		"work correctly for zero latitude and longitude" in {
+			val geoPoint = GeoPoint.parse(0, 0)
+			geoPoint.asDms must be equalTo("0° 0' 0\" 0° 0' 0\"")
+		}
+
+		"be able to output localized results" in {
+			val geoPoint = GeoPoint.parse(-33.855270, 151.209725)
+			geoPoint.asDms(Locale.GERMAN) must be equalTo("S33° 51' 18,972\" E151° 12' 35,01\"")
+		}
+
+	}
+
+	def after {
+		Locale.setDefault(defaultLocale)
 	}
 
 }

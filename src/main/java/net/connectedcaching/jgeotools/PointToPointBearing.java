038626cc5ea5c3db540eef127163a4461af27af6
Leo Schweizer <leonhard.schweizer@googlemail.com>
2012-10-01 20:53:38 +0200

    removed unused code

--- src/main/java/net/connectedcaching/jgeotools/PointToPointBearing.java
+++ src/main/java/net/connectedcaching/jgeotools/PointToPointBearing.java
@@ -10,16 +10,6 @@ public class PointToPointBearing extends Bearing {
 	protected GeoPoint from;
 	protected GeoPoint to;
 
-	/*var lat1 = this._lat.toRad(), lat2 = point._lat.toRad();
-	var dLon = (point._lon-this._lon).toRad();
-
-	var y = Math.sin(dLon) * Math.cos(lat2);
-	var x = Math.cos(lat1)*Math.sin(lat2) -
-			Math.sin(lat1)*Math.cos(lat2)*Math.cos(dLon);
-	var brng = Math.atan2(y, x);
-
-	return (brng.toDeg()+360) % 360;*/
-
 	private Double getBearing() {
 		Double longitudeDistance = Math.toRadians(to.getLongitude() - from.getLongitude());
 		Double x = Math.cos(from.getLatitudeRadians()) * Math.sin(to.getLatitudeRadians()) -
